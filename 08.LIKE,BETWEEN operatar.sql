b17e238747cdf51dbc43f8f215cf741e8bd4fa3e
Madan H M <madan3.h.m@gmail.com>
2024-10-24 22:17:13 +0530

    added

---
+++ 08.LIKE,BETWEEN operatar.sql
@@ -0,0 +1,8 @@
+--LIKE OPERATAR--
+SELECT * FROM employee WHERE e_name LIKE't%';
+
+SELECT * FROM employee WHERE e_age LIKE '4_';
+--BETWEEN OPERATAR--
+SELECT * FROM employee WHERE e_age BETWEEN 30 AND 50;
+SELECT * FROM employee WHERE e_salary BETWEEN 30000 AND 60000;
+
