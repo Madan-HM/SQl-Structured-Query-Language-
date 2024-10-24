c7239451c6c9eb9e7f0f031085697456437975d9
Madan H M <madan3.h.m@gmail.com>
2024-10-24 21:46:02 +0530

    added

---
+++ 03.insert values or records into the table.sql
@@ -0,0 +1,21 @@
+USE sqldatabase;
+INSERT INTO Employee values(
+    1,'sam',95000,45,'male','operations'
+);
+INSERT INTO Employee values(
+    2,'tam',55000,45,'female','sales'
+);
+
+INSERT INTO Employee values(
+    3,'ram',35000,45,'male','analytics'
+);
+INSERT INTO Employee values(
+    4,'bam',65000,45,'female','sales'
+);
+INSERT INTO Employee values(
+    5,'xam',75000,45,'male','marketing'
+);
+INSERT INTO Employee values(
+    6,'dam',75000,45,'female','operations'
+);
+
