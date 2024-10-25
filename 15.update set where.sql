b68ab2888e23e9d9f5ee2c22d4c55e12f3ba3702
Madan H M <madan3.h.m@gmail.com>
2024-10-25 18:44:32 +0530

    added

---
+++ 15.update set where.sql
@@ -0,0 +1,14 @@
+UPDATE student1
+SET amrks="0"
+WHERE grade ="A";
+
+SELECT* FROM student1;
+
+UPDATE student1
+SET marks="92"
+WHERE name ="EMANUEL";
+
+UPDATE student1
+SET grade ="F"
+WHERE marks BETWEEN 80 AND 90;
+
