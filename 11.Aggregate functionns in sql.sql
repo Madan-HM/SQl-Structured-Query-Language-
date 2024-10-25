a06f60a6aa2c5000b7431a01a9d2a33817cfcb4d
Madan H M <madan3.h.m@gmail.com>
2024-10-25 16:12:36 +0530

    added

---
+++ 11.Aggregate functionns in sql.sql
@@ -0,0 +1,7 @@
+SELECT * FROM student1;
+SELECT MAX(marks) FROM student1;
+SELECT MIN(marks) FROM student1;
+SELECT SUM(marks) FROM student1;
+SELECT AVG(marks) FROM student1;
+SELECT COUNT(rollno) FROM student1;
+
