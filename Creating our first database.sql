8c24957feaf93bb12bbae840a220543622df0049
Madan H M <madan3.h.m@gmail.com>
2024-10-25 12:23:28 +0530

    added

---
+++ Creating our first database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE temp1; -- to create database
+
+create database temp2;
+
+CREATE DATABASE college;
+
+DROP DATABASE temp1; --to delete database
+
+DROP DATABASE temp2;
+
+USE college; --using perticular database to perfroms operations 
